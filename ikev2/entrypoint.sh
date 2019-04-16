14e1520e1f149b96732253349d27386274919a20
panhaipeng <panhaipeng@fedora-at-home.localdomain>
2019-04-16 22:26:20 +0800

    fix ikev2 mtu issue when use linux client

--- ikev2/entrypoint.sh
+++ ikev2/entrypoint.sh
@@ -8,7 +8,12 @@ set -e
 iptables -t nat -A PREROUTING -d $SS_SERVER_HOST -j RETURN
 
 # Make strongswan can access network
-iptables -t nat -A POSTROUTING -s 10.2.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -j MASQUERADE
+
+# When use Linux Strongswan Client Or Android Client the MSS may consensus to be 1460, then the client can not access some website(eg. https://baidu.com).
+# IOS client do not have this problem.
+# MSS set to 1360
+iptables -t mangle -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
 
 # Update iptables to split CHN-IPs
 /bin/bash /update-iptables.sh
