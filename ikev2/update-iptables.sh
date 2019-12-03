5045f1920dfd11591c3cd7b838854f317ef9542b
panhaipeng <panhaipeng@fedora-at-home.localdomain>
2019-12-03 15:56:10 +0800

    Update ikev2 iptable for v2ray.

--- ikev2/update-iptables.sh
+++ ikev2/update-iptables.sh
@@ -24,51 +24,67 @@ reserved_networks=(\
   "240.0.0.0/4")
 
 
-# ================================== TCP ===============================
-# Create chain if not exist
-iptables -t nat -n -L SHADOWSOCKS >/dev/null 2>&1 \
-  || (iptables -t nat -N SHADOWSOCKS && iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS)
+IN_PROXY_CHAIN="MY_IN_PROXY"
+IN_PROXY_TABLE="mangle"
+OUT_MARK_CHAIN="MY_OUT_MARK"
+OUT_MARK_TABLE="mangle"
 
-# Flush chain
-iptables -t nat -F SHADOWSOCKS
+# Proxy chain for VPN client
+iptables -t $IN_PROXY_TABLE -n -L $IN_PROXY_CHAIN >/dev/null 2>&1 \
+  || (iptables -t $IN_PROXY_TABLE -N $IN_PROXY_CHAIN && iptables -t $IN_PROXY_TABLE -A PREROUTING -j $IN_PROXY_CHAIN)
 
-# All reserved network bypass the proxy
+## Flush IN_PROXY_CHAIN
+iptables -t $IN_PROXY_TABLE -F $IN_PROXY_CHAIN
+
+## All reserved network bypass the proxy
 for i in ${reserved_networks[@]}; do
-  iptables -t nat -A SHADOWSOCKS -d $i -j RETURN
+  iptables -t $IN_PROXY_TABLE -A $IN_PROXY_CHAIN -d $i -j RETURN
 done
 
-# All CHN IPs will bypass the proxy
+## All CHN IPs will bypass the proxy
 for i in $(cat /CHN-IPs)
 do
-  iptables -t nat -A SHADOWSOCKS -d $i -j RETURN
+  iptables -t $IN_PROXY_TABLE -A $IN_PROXY_CHAIN -d $i -j RETURN
 done
 
-# Anything else should be redirected to shadowsocks's local port
-iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports 1080
+## Anything else should be proxy
+
+### TCP proxy
+iptables -t $IN_PROXY_TABLE -A $IN_PROXY_CHAIN -p tcp -j TPROXY --on-port 1080 --tproxy-mark 1
+
+### UDP proxy( only proxy dns, port 53)
+iptables -t $IN_PROXY_TABLE -A $IN_PROXY_CHAIN -p udp --dport 53 -j TPROXY --on-port 1080 --tproxy-mark 0x01/0x01
 
 
-# ================================== UDP ===============================
-# Create chain if not exits
-iptables -t mangle -n -L SHADOWSOCKS >/dev/null 2>&1 \
-  || (iptables -t mangle -N SHADOWSOCKS \
-    && iptables -t mangle -A PREROUTING -p udp --dport 53 -j SHADOWSOCKS \
+
+# Proxy local machine traffic
+iptables -t $OUT_MARK_TABLE -n -L $OUT_MARK_CHAIN >/dev/null 2>&1 \
+  || (iptables -t $OUT_MARK_TABLE -N $OUT_MARK_CHAIN \
+    && iptables -t $OUT_MARK_TABLE -A OUTPUT -p udp --dport 53 -j $OUT_MARK_CHAIN\
     && ip route add local default dev lo table 100 \
-    && ip rule add fwmark 1 lookup 100)
+    && ip rule add fwmark 1 table 100)
 
-# Flush chain
-iptables -t mangle -F SHADOWSOCKS
 
-# All reserved network bypass the proxy
+## Flush OUT_MARK_CHAIN
+iptables -t $OUT_MARK_TABLE -F $OUT_MARK_CHAIN
+
+## Marked by 0xff(255) is from the v2ray, bypass the proxy
+iptables -t $OUT_MARK_TABLE -A $OUT_MARK_CHAIN -m mark --mark 0xff -j RETURN
+
+## All reserved network bypass the proxy
 for i in ${reserved_networks[@]}; do
-  iptables -t mangle -A SHADOWSOCKS -d $i -j RETURN
+  iptables -t $OUT_MARK_TABLE -A $OUT_MARK_CHAIN -d $i -j RETURN
 done
 
-# All CHN IPs will bypass the proxy
+## All CHN IPs will bypass the proxy
 for i in $(cat /CHN-IPs)
 do
-  iptables -t mangle -A SHADOWSOCKS -d $i -j RETURN
+  iptables -t $OUT_MARK_TABLE -A $OUT_MARK_CHAIN -d $i -j RETURN
 done
 
-# Any DNS query (destination dns server not in CHN) will go through proxy, to prevent DNS cache pollution. TODO: (write a description about this)
-iptables -t mangle -A SHADOWSOCKS -p udp --dport 53 -j TPROXY --on-port 1080 --tproxy-mark 0x01/0x01
+### Mark TCP, and re-routing
+iptables -t $OUT_MARK_TABLE -A $OUT_MARK_CHAIN -p tcp -j MARK --set-mark 1
+
+### UDP proxy. Any DNS query (destination dns server not in CHN) will go through proxy, to prevent DNS cache pollution. TODO: (write a description about this)
+iptables -t $OUT_MARK_TABLE -A $OUT_MARK_CHAIN -p udp --dport 53 -j MARK --set-mark 1 # Mark and re-routing
 
